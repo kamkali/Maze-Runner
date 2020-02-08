1ead11bd5022a72429d2a2c980f884354da0f366
kamkali <kamkalis@gmail.com>
2020-02-08 18:02:16 +0100

    MazeGenerator: progressing in algorithm implementation

--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MazeGenerator implements Maze {
 
     private Cell[][] arr2D;
@@ -15,6 +19,43 @@ public class MazeGenerator implements Maze {
         }
         currentCell = arr2D[0][0];
         currentCell.setVisited(true);
+    }
+
+    public boolean checkIndex(int row, int col){
+        return !(row < 0 || row > this.arr2D.length || col < 0 || col > this.arr2D[0].length);
+    }
+
+    public Cell checkNeighbors(Cell[][] grid, int row, int col){
+        List<Cell> unvisitedNeighbors = new ArrayList<>();
+        Cell top;
+        Cell right;
+        Cell bottom;
+        Cell left;
+
+
+        if (checkIndex(row - 1, col) || !grid[row - 1][col].isVisited()) {
+            top = grid[row - 1][col];
+            unvisitedNeighbors.add(top);
+        }
+        if (checkIndex(row, col + 1) || !grid[row][col + 1].isVisited()) {
+            right = grid[row][col + 1];
+            unvisitedNeighbors.add(right);
+        }
+        if (checkIndex(row + 1, col) || !grid[row + 1][col].isVisited()) {
+            bottom = grid[row + 1][col];
+            unvisitedNeighbors.add(bottom);
+        }
+        if (checkIndex(row, col - 1) || !grid[row][col - 1].isVisited()) {
+            left = grid[row][col - 1];
+            unvisitedNeighbors.add(left);
+        }
+
+        if (!unvisitedNeighbors.isEmpty()){
+            int randomNum = ThreadLocalRandom.current().nextInt(0, unvisitedNeighbors.size() + 1);
+            return unvisitedNeighbors.get(randomNum);
+        } else {
+            return null;
+        }
     }
 
 //    @Override
